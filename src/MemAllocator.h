a182be8ca7c9b265cc8d0c3b8ea4994ee90cf2dd
Ozan <ozan.gunaydin@incyton.com>
2022-11-01 23:27:31 +0100

    update header documentation

--- src/MemAllocator.h
+++ src/MemAllocator.h
@@ -1,19 +1,35 @@
 #ifndef _MEMALLOCATOR_H_
 #define _MEMALLOCATOR_H_
 
-//Allocate up to "count" blocks each of "size" bytes
-//Init must be called first.
+/*
+ *  allocates up to count blocks each of size bytes
+ *  Must be called first before other API functions
+ *  param:
+ *  count: number of blocks in the list
+ *  size: size of the data type
+ *  return:
+ * 
+ */
 void init(int count,int size);
 
-//Returns a pointer to one of the "count" blocks set up by Init(). If no
-//blocks remain, return NULL
-//Must execute in constant time O(1)
+
+/*
+ *  Returns a pointer to one of the "count" blocks set up by Init()
+ *  If no blocks remain returns NULL
+ *  param:
+ *  
+ *  return:
+ *  void* points to a free block
+ */
 void* Allocate(void);
 
 
-//Returns a block allocated by "Allocate()" to the block pool. Release()
-//may assume pBlock is not NULL and was returned by Allocate().
-//Must execute in constant time O(1)
+/*
+ *  Returns a block allocated by "Allocate()" to the block pool.
+ *  param:
+ *  *pBlock pointer to block to be released
+ *  return:
+ */
 void Release(void *pBlock);
 
 #endif  // _MEMALLOCATOR_H_
